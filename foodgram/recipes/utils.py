aa5dfa6e3b875b09515d5ed0e927c93fc207c9c7
roman1916 <shakhabkhan1916@gmail.com>
2021-10-09 22:19:26 +0300

    ..

--- foodgram/recipes/utils.py
+++ foodgram/recipes/utils.py
@@ -6,18 +6,16 @@ from .models import RecipeIngredient
 def get_ingredients_list(recipes_list):
     ingredients_dict = {}
     for recipe in recipes_list:
-        ingredients = RecipeIngredient.objects.filter(recipe=recipe.recipe)
-        for ingredient in ingredients:
-            amount = ingredient.amount
-            name = ingredient.ingredient.name
-            measurement_unit = ingredient.ingredient.measurement_unit
-            if name not in ingredients_dict:
-                ingredients_dict[name] = {
-                    'measurement_unit': measurement_unit,
-                    'amount': amount
-                }
-            else:
-                ingredients_dict[name]['amount'] += amount
+        amount = recipe.amount
+        name = recipe.ingredient.name
+        measurement_unit = recipe.ingredient.measurement_unit
+        if name not in ingredients_dict:
+            ingredients_dict[name] = {
+                'measurement_unit': measurement_unit,
+                'amount': amount
+            }
+        else:
+            ingredients_dict[name]['amount'] += amount
     to_buy = []
     for item in ingredients_dict:
         to_buy.append(f'{item} - {ingredients_dict[item]["amount"]} '
