c249253d46decb493e84d1a7d17043b444e72bc3
roman1916 <shakhabkhan1916@gmail.com>
2021-10-10 22:09:37 +0300

    after...

--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -1,24 +1,19 @@
 from django.contrib.auth import get_user_model
-from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.lib import colors
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
 from .filters import IngredientsFilter, RecipeFilter
 from .mixins import RetriveAndListViewSet
-from .models import (Favorite, Ingredient, Recipe,
-                     RecipeIngredient, Tag, ShoppingList)
+from .models import Favorite, Ingredient, Recipe, ShoppingList, Tag
 from .pagination import CustomPageNumberPaginator
 from .permissions import IsAuthorOrAdmin
 from .serializers import (AddRecipeSerializer, FavouriteSerializer,
                           IngredientsSerializer, ShoppingListSerializer,
                           ShowRecipeFullSerializer, TagsSerializer)
+from .utils import download_file_response, get_ingredients_list
 
 User = get_user_model()
 
@@ -89,48 +84,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     @action(detail=False, permission_classes=[permissions.IsAuthenticated])
     def download_shopping_cart(self, request):
-        recipe = request.user.shopping_list.all()
-        shopping_list1 = {}
-        ingredients = RecipeIngredient.objects.filter(
-            recipe=recipe).values_list(
-                'ingredient__name',
-                'amount',
-                'ingredient__measurement_unit',
-                named=True)
-        for ingredient in ingredients:
-            name = ingredient.ingredient__name
-            measurement_unit = ingredient.ingredient__measurement_unit
-            amount = ingredient.amount
-            if name not in shopping_list1:
-                shopping_list1[name] = {
-                    'measurement_unit': measurement_unit,
-                    'amount': amount
-                }
-            else:
-                shopping_list1[name]['amount'] += amount
-        file_name = 'ShoppingList1'
-        response = HttpResponse(content_type='application/pdf')
-        content_disposition = f'attachment; filename="{file_name}.pdf"'
-        response['Content-Disposition'] = content_disposition
-        pdfmetrics.registerFont(TTFont('Neocyr', 'Neocyr.ttf', 'UTF-8'))
-        pdf = canvas.Canvas(response)
-        pdf.setFont('Neocyr', 24)
-        pdf.setFillColor(colors.black)
-        pdf.drawCentredString(300, 770, 'Список покупок')
-        pdf.setFillColor(colors.black)
-        pdf.setFont('Neocyr', 16)
-        height = 700
-        for name, data in shopping_list1.items():
-            pdf.drawString(
-                60,
-                height,
-                f"{name} - {data['amount']} {data['measurement_unit']}"
-            )
-            height -= 25
-            if height == 50:
-                pdf.showPage()
-                pdf.setFont('Neocyr', 16)
-                height = 700
-        pdf.showPage()
-        pdf.save()
-        return response
+        user_shopping_list = request.user.shopping_list.all()
+        to_buy = get_ingredients_list(user_shopping_list)
+        return download_file_response(to_buy, 'to_buy.txt')
