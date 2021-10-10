a9d6be35b33b16c8ed5163b7e5ee5ce02948049d
roman1916 <shakhabkhan1916@gmail.com>
2021-10-10 15:23:49 +0300

    .

--- foodgram/recipes/views.py
+++ foodgram/recipes/views.py
@@ -85,6 +85,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
     @action(detail=False, permission_classes=[permissions.IsAuthenticated])
     def download_shopping_cart(self, request):
         recip = request.user.shopping_cart.recipes.all().values_list('ingredient__name',
-                    'amount', 'ingredient__measurement_unit')
+            'amount', 'ingredient__measurement_unit')
         to_buy = get_ingredients_list(recip)
         return download_file_response(to_buy, 'to_buy.txt')
